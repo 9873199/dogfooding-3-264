3b60f6a6a3d465489662daf9d94e98f6ef5aa3fc
9873199 <9873199@qq.com>
2026-04-21 03:36:55 +0800

    kimi commit

--- environment/repo/vue.config.js
+++ environment/repo/vue.config.js
@@ -6,6 +6,11 @@ module.exports = {
   publicPath: './',
   chainWebpack: config => {
     config.resolve.alias
-      .set('_c', resolve('src/components')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('_c', resolve('src/components'))
   },
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+    open: true
+  }
 }
