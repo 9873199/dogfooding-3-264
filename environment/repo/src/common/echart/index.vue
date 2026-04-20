cbc8fdc2c50d116378a0684d4b50d4ba10b84603
9873199 <9873199@qq.com>
2026-04-21 04:41:46 +0800

    glm commit

--- environment/repo/src/common/echart/index.vue
+++ environment/repo/src/common/echart/index.vue
@@ -34,27 +34,45 @@ export default {
   data () {
     return {
       chart: null,
-      resizeHandler: null
+      resizeHandler: null,
+      isDisposed: false
     }
   },
   watch: {
     options: {
       handler (options) {
+        if (this.isDisposed) return
         if (this.chart) {
-          this.chart.setOption(options, true)
+          this.chart.setOption(options, { notMerge: false, lazyUpdate: false })
+          this.$nextTick(() => {
+            if (this.chart && !this.isDisposed) {
+              this.chart.resize()
+            }
+          })
+        } else {
+          this.initChart()
         }
       },
-      deep: true,
-      immediate: true
+      deep: true
     }
   },
   mounted () {
     this.$echarts.registerTheme('tdTheme', tdTheme)
+    this.isDisposed = false
     this.initChart()
     this.initResizeEvent()
   },
   activated () {
-    if (this.chart) {
+    if (this.chart && !this.isDisposed) {
+      this.$nextTick(() => {
+        if (this.chart && !this.isDisposed) {
+          this.chart.resize()
+        }
+      })
+    }
+  },
+  deactivated () {
+    if (this.chart && !this.isDisposed) {
       this.chart.resize()
     }
   },
@@ -63,28 +81,35 @@ export default {
   },
   methods: {
     initChart () {
+      if (this.isDisposed) return
       if (this.chart) {
         this.chart.dispose()
+        this.chart = null
       }
       this.chart = this.$echarts.init(this.$el, 'tdTheme')
       if (this.options && Object.keys(this.options).length > 0) {
-        this.chart.setOption(this.options, true)
+        this.chart.setOption(this.options, { notMerge: true })
       }
     },
     initResizeEvent () {
+      if (this.resizeHandler) {
+        window.removeEventListener('resize', this.resizeHandler)
+      }
       this.resizeHandler = debounce(() => {
-        if (this.chart) {
+        if (this.chart && !this.isDisposed) {
           this.chart.resize()
         }
       }, 300)
       window.addEventListener('resize', this.resizeHandler)
     },
     destroyChart () {
+      this.isDisposed = true
       if (this.resizeHandler) {
         window.removeEventListener('resize', this.resizeHandler)
         this.resizeHandler = null
       }
       if (this.chart) {
+        this.chart.clear()
         this.chart.dispose()
         this.chart = null
       }
--- environment/repo/src/utils/drawMixin.js
+++ environment/repo/src/utils/drawMixin.js
@@ -12,7 +12,8 @@ const baseProportion = parseFloat((baseWidth / baseHeight).toFixed(5))
 export default {
   data() {
     return {
-      drawTiming: null
+      drawTiming: null,
+      isFullScreen: false
     }
   },
   mounted () {
@@ -20,11 +21,13 @@ export default {
     window.addEventListener('resize', this.resize)
     window.addEventListener('fullscreenchange', this.handleFullScreenChange)
     document.addEventListener('webkitfullscreenchange', this.handleFullScreenChange)
+    window.addEventListener('orientationchange', this.handleOrientationChange)
   },
   beforeDestroy () {
     window.removeEventListener('resize', this.resize)
     window.removeEventListener('fullscreenchange', this.handleFullScreenChange)
     document.removeEventListener('webkitfullscreenchange', this.handleFullScreenChange)
+    window.removeEventListener('orientationchange', this.handleOrientationChange)
     if (this.drawTiming) {
       clearTimeout(this.drawTiming)
       this.drawTiming = null
@@ -38,27 +41,36 @@ export default {
       if (currentRate > baseProportion) {
         scale.width = ((window.innerHeight * baseProportion) / baseWidth).toFixed(5)
         scale.height = (window.innerHeight / baseHeight).toFixed(5)
-        appRef.style.transform = `scale(${scale.width}, ${scale.height}) translate(-50%, -50%)`
       } else {
         scale.height = ((window.innerWidth / baseProportion) / baseHeight).toFixed(5)
         scale.width = (window.innerWidth / baseWidth).toFixed(5)
-        appRef.style.transform = `scale(${scale.width}, ${scale.height}) translate(-50%, -50%)`
       }
+      appRef.style.transform = `scale(${scale.width}, ${scale.height}) translate(-50%, -50%)`
+      appRef.style.transformOrigin = '0 0'
     },
     resize () {
       clearTimeout(this.drawTiming)
       this.drawTiming = setTimeout(() => {
         this.calcRate()
-        this.triggerChartResize()
+        this.$nextTick(() => {
+          this.triggerChartResize()
+        })
       }, 200)
     },
     handleFullScreenChange () {
+      this.isFullScreen = !!(document.fullscreenElement || document.webkitFullscreenElement)
       this.$nextTick(() => {
         this.calcRate()
         setTimeout(() => {
           this.triggerChartResize()
-        }, 100)
+        }, 150)
       })
+    },
+    handleOrientationChange () {
+      setTimeout(() => {
+        this.calcRate()
+        this.triggerChartResize()
+      }, 200)
     },
     triggerChartResize () {
       const charts = document.querySelectorAll('div[_echarts_instance_]')
